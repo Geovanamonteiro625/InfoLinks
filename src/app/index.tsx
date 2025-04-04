aaccf3ecfaf74daac3bfe5e549a0ecd82ca26669
Geovana Monteiro <manteirogeovana@gmail.com>
2025-04-04 20:51:01 -0300

    primeiro comit

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import {View,Text} from "react-native";
+
+export default function Index () {
+    return (
+        <View>
+            <Text> eu sou o milior!!! </Text>
+        </View>
+    
+    );
+
+        
+    }  
+    
+    
