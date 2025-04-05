fded01847cc8170e2d6125448c18fe17ebcaa40d
Geovana Monteiro <manteirogeovana@gmail.com>
2025-04-05 15:22:23 -0300

    Inserindo novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,17 @@ import {View,Text} from "react-native";
 
 export default function Index () {
     return (
+        // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota
         <View>
             <Text> eu sou o milior!!! </Text>
+            <Text> Eu sou a melhor</Text> 
+            <Text> O texto reative</Text>
         </View>
-    
+     
     );
 
         
     }  
+    
     
     
