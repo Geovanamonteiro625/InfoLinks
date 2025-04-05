568c0fffea2bf1bc005481492491a1c0aac9b704
Geovana Monteiro <manteirogeovana@gmail.com>
2025-04-05 17:33:18 -0300

    adicionando estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,44 @@
-import {View,Text} from "react-native";
+import {View,Text, StyleSheet} from "react-native";
 
 export default function Index () {
     return (
         // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota
         <View>
-            <Text> eu sou o milior!!! </Text>
-            <Text> Eu sou a melhor</Text> 
-            <Text> O texto reative</Text>
-            <Text> 10/02</Text>
-            <Text> Geovana monteiro</Text>
-            <Text> Erro na coluna </Text>
-            <Text> Garantia de um android</Text>
+            <Text style={style.title}>eu sou o milior!!! </Text>
+            <Text style={}> Eu sou a melhor</Text> 
+            <Text style={style.title1}> O texto reative</Text>
+            <Text style={style.title2}> 10/02</Text>
+            <Text style={style.title3}> Geovana monteiro</Text>
+            <Text style={ style.title4}> Erro na coluna </Text>
+            <Text style={style.title5}> Garantia de um android</Text>
         </View>
      
-    );
+    );  
+}  
+      
+const style = StyleSheet.create({
+    title:{
+        color:"red",
+        fontSize: 22,
+    },
+    title1: {
+        color:"blue",
+        fontSize:26
+    },
+    title2:{
+     color:"red",
+    fontSize:22,
+    },
 
-        
-    }  
-    
+    title3:{
+        color:"red",
+        fonSize: 22
+    },
+    title4:{
+        color:"red",
+        title:
+    }
+
+  })
     
     
