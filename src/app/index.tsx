e74ddb4f4e8a26716fca911a5be4ea27d8077dc2
Geovana Monteiro <manteirogeovana@gmail.com>
2025-04-11 20:47:15 -0300

    novoa elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,20 +3,26 @@ import {View,Text, StyleSheet} from "react-native";
 export default function Index () {
     return (
         // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota
-        <View>
-            <Text style={style.title}>eu sou o milior!!! </Text>
-            <Text style={}> Eu sou a melhor</Text> 
-            <Text style={style.title1}> O texto reative</Text>
-            <Text style={style.title2}> 10/02</Text>
-            <Text style={style.title3}> Geovana monteiro</Text>
-            <Text style={ style.title4}> Erro na coluna </Text>
-            <Text style={style.title5}> Garantia de um android</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>eu sou o milior!!! </Text>
+            <Text style={styles.title}> Eu sou a melhor</Text> 
+            <Text style={styles.title1}> O texto reative</Text>
+            <Text style={styles.title2}> 10/02</Text>
+            <Text style={styles.title3}> Geovana monteiro</Text>
+            <Text style={ styles.title4}> Erro na coluna </Text>
+            <Text style={styles.title5}> Garantia de um android</Text>
         </View>
-     
+    
     );  
 }  
       
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
+    container:{
+        flex:1, // considera toda a area util da tela para os componentes
+        justifyContent: "center", //justifica(ajusta) todos os elementos no centro da
+        alignItems: "center", //Alinha todos os elementos centro da tela 
+        flexDirection:"row", //Organiza todos os elementos dentro doflex box em linha (esquerda)
+    },
     title:{
         color:"red",
         fontSize: 22,
@@ -32,13 +38,17 @@ const style = StyleSheet.create({
 
     title3:{
         color:"red",
-        fonSize: 22
+        fontSize:22,
     },
     title4:{
         color:"red",
-        title:
-    }
+        fontSize:22,
+    },
 
+    title5:{
+        color:"red",
+        fontSize:22, 
+    },
   })
     
     
